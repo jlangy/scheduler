9872fd1c4ffb01eafd884ced2a04d4caa76542af
Jon Langlois <jonathan-langlois@live.ca>
2020-02-19 18:44:09 -0800

    Make spots update. Fix no interviewer error

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,12 +19,18 @@ const CONFIRM = "CONFIRM";
 const EDITING = "EDITING";
 const ERROR_SAVE = "ERROR_SAVE";
 const ERROR_DELETE = "ERROR_DELETE";
+const ERROR_NO_INTERVIEWER = "ERROR_NO_INTERVIEWER";
 
 
 export default function Appointment(props){
   const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY);
 
   function save(name, interviewer){
+
+    if(!interviewer){
+      return transition(ERROR_NO_INTERVIEWER) 
+    }
+
     const interview = {
       student:name,
       interviewer
@@ -56,6 +62,7 @@ export default function Appointment(props){
         {mode === DELETING && <Status  message={DELETING}/>}
         {mode === ERROR_SAVE && <Error  message={"Server error while saving. Please try again"} onClose={back}/>}
         {mode === ERROR_DELETE && <Error  message="Server error while deleting. Please try again" onClose={back}/>}
+        {mode === ERROR_NO_INTERVIEWER && <Error  message="You must select an interviewer to book an appointment" onClose={back}/>}
         {mode === CONFIRM && <Confirm  onCancel={back} onConfirm={cancel} message="Are you sure you want to delete?" />}
       </article>
   )
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,41 @@
-import {useState, useEffect} from "react";
+import {useReducer, useEffect} from "react";
 import axios from "axios";
 
 export default function useApplicationData(){
+  
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+  const ADD_SPOT = "ADD_SPOT";
+  const REMOVE_SPOT = "REMOVE_SPOT";
 
-  const [state, setState] = useState({days: [], appointments: {}, interviewers: {}, day: ""});
+  const reducer = (state, action) => {
+    switch(action.type){
+      case SET_DAY:
+        return {...state, ...action.value}
+        case SET_APPLICATION_DATA:
+          return {...state, ...action.value}
+        case SET_INTERVIEW: {
+          return {...state, ...action.value}
+        }
+        case ADD_SPOT: {
+          //Increment spots for the day with current days name
+          const updatedDays = state.days.map(day => day.name === state.day ? {...day, spots: day.spots + 1} : day);
+          return {...state, days: updatedDays}
+        }
+        case REMOVE_SPOT: {
+          const updatedDays = state.days.map(day => day.name === state.day ? {...day, spots: day.spots - 1} : day);
+          return {...state, days: updatedDays}
+        }
+        default:
+          throw new Error(
+            `Tried to reduce with unsupported action type: ${action.type}`
+          );
+      }
+    }
+
+  const [state, dispatch] = useReducer(reducer, {days: [], appointments: {}, interviewers: {}, day: ""});
+
   
   useEffect(() => {
     const daysPromise = axios.get("/api/days");
@@ -11,16 +43,19 @@ export default function useApplicationData(){
     const interviewersPromise = axios.get("/api/interviewers");
     Promise.all([daysPromise, appointmentsPromise, interviewersPromise])
     .then(([daysResponse, appointmentsResponse, interviewersResponse]) => {
-      setState({day: "", 
-      days: daysResponse.data, 
-      appointments: appointmentsResponse.data, 
-      interviewers:interviewersResponse.data});
+      dispatch({
+        type: SET_APPLICATION_DATA, 
+        value: {
+          days: daysResponse.data, 
+          appointments: appointmentsResponse.data, 
+          interviewers:interviewersResponse.data}});
     });
   }, [])
   
-  const setDay = day => setState({...state, day});
+  const setDay = day => dispatch({type: SET_DAY, value: {day}});
 
   function bookInterview(id, interview){
+
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -35,7 +70,14 @@ export default function useApplicationData(){
     return axios.put(`/api/appointments/${id}`, {
       interview
     })
-      .then(res => setState({...state, appointments}))
+      .then(res => {
+
+        if(!state.appointments[id].interview){
+          dispatch({type: REMOVE_SPOT})
+        }
+
+        dispatch({type: SET_INTERVIEW, value: {appointments}})
+      });
   }
   
   function cancelInterview(id){
@@ -50,8 +92,10 @@ export default function useApplicationData(){
     };
     
     return axios.delete(`/api/appointments/${id}`)
-      .then(res => setState({...state, appointments}))  
+      .then(res => {
+          dispatch({type: ADD_SPOT});
+          dispatch({type: SET_INTERVIEW, value: {appointments}})
+        });  
   }
-
   return {state, setDay, bookInterview, cancelInterview}
 }
